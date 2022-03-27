e3292661c4365764873d7ba536642efd9044a921
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-03-28 01:09:13 +0530

    added Midleware for loggedin , flash msg for seccessful or error log in ,signup , backend user authentication

---
+++ middleware.js
@@ -0,0 +1,8 @@
+module.exports.isLoggedIn = (req,res,next) => {
+    if(!req.isAuthenticated()){ 
+        req.session.returnto = req.originalUrl;
+        req.flash('error','You Must be Signed in First');
+        return res.redirect('/login');
+    }
+    next();
+}
