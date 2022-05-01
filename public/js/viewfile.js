0306a284b1a85066093449e36a9cce72fabd2789
Saurabh Katwal <saurabhkatwal@gmail.com>
2022-05-01 12:51:17 +0530

    added book button and enabled it to display some text

---
+++ public/js/viewfile.js
@@ -0,0 +1,6 @@
+let bookbtn=document.querySelector('.btn-success');
+let text=document.querySelector('#txt-print');
+bookbtn.addEventListener('click',()=>{
+    text.style.display='block';
+text.innerHTML="Please contact with the owner: Name:adsfa Contact: 124124142";
+})
