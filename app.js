5bfa2da27cdc5e4f542ffe1347c50459df9cae76
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-05-25 16:09:42 +0530

    Add Start Script

--- app.js
+++ app.js
@@ -457,6 +457,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("Error", { err });
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
   console.log("App Stated");
 });
